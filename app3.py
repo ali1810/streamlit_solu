531c4b026a1ef30cf26c8df43c5872d31255d429
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-01-24 16:01:13 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -5,7 +5,7 @@ from PIL import Image
 def page1():
     #st.title("AqSolPred: Online Solubility Prediction Tool")
     
-    st.markdown("<h1 style='text-align: left;position: fixed;  width: 100%; color: blue; margin-top: -1; padding-top: -1;'>AqSolPred: Online Solubility Prediction Tool</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: left;position: fixed;  top: 0; width: 100%; color: blue; margin-top: -1; padding-top: -1;'>AqSolPred: Online Solubility Prediction Tool</h1>", unsafe_allow_html=True)
     #st.markdown("<h1 style='text-align: left; color: blue; position: fixed;margin-top: -1; padding-top: -1; width: 100%;
     #'>AqSolPred: Online Solubility Prediction Tool</h1>", unsafe_allow_html=True)
 	
