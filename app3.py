7c8cf8a274e008d9b38a2f427c235dd4cf1fef71
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-01-24 14:32:08 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -5,23 +5,18 @@ def page1():
     #st.write("This is the content of Page 1.")
     #st.set_page_config(page_title="AqSolPred: Online Solubility Prediction Tool",layout="wide")
     st.write("""# Solibility Prediction on Aqueous Solvent """)
-    image = Image.open('Flow.jpeg')
-    col1, col2, col3 = st.columns([0.5,2.0,0.5])
+    
     image = Image.open('Flow.jpeg')
     col1, col2, col3 = st.columns([0.5,2.0,0.5])
     with col1:
 	st.write("")
-
     with col2:
-	
         st.image(image, use_column_width=6)
     with col3:	
         st.write("")
-
 def page2():
     st.title("Page 2")
     st.write("This is the content of Page 2.")
-    
 def page3():
     st.title("Page 3")
     st.write("This is the content of Page 3.")
@@ -36,6 +31,5 @@ def main():
         page2()
     elif selected_page == "Page 3":
         page3()
-
 if __name__ == "__main__":
     main()
