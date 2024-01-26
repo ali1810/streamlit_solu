2d748b55a0b361ed48fa28a76690e64b7c27771f
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-01-26 13:21:14 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -43,10 +43,10 @@ def page1():
     #SMILES_input = "CN1C=NC2=C1C(=O)N(C(=O)N2C)C"
     #\nCC(=O)OC1=CC=CC=C1C(=O)O"
     #SMILES_input = " "
-        smiles = st.sidebar.text_input('then press predict button', value ="CC(=O)OC1=CC=CC=C1C(=O)O")
+        smiles1 = st.sidebar.text_input('then press predict button', value ="CC(=O)OC1=CC=CC=C1C(=O)O")
     #SMILES = SMILES.split('\n')
    #col1, col2, col3 = st.columns([10,2,11.5]) 
-	prop=pcp.get_properties([ 'MolecularWeight'], smiles, 'smiles')
+	prop=pcp.get_properties([ 'MolecularWeight'], smiles1, 'smiles')
         x = list(map(lambda x: x["CID"], prop))
         y=x[0]
     #print(y)
