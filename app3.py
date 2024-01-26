f3a1474aaf1676878d4e588ef6fb50c35ab6d61c
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-01-26 13:04:28 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -33,7 +33,21 @@ def render_mol(xyz):
     showmol(xyzview,height=300,width=400) 
 
 def page1():
-    #st.title("AqSolPred: Online Solubility Prediction Tool")
+    
+    def smiles_to_img(SMILES):
+         prop=pcp.get_properties([ 'MolecularWeight'], SMILES, 'smiles')
+         x = list(map(lambda x: x["CID"], prop))
+         y=x[0]
+    #print(y)
+         x = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/PNG?image_size=400x300"
+         url=(x % y)
+#print(url)
+        img = Image.open(urlopen(url))
+    return img 
+	
+	
+	
+	#st.title("AqSolPred: Online Solubility Prediction Tool")
     
     #st.markdown("<h1 style='text-align: left;position: fixed;  top: 0; width: 100%; color: blue; margin-top: -1; padding-top: -1;'>AqSolPred: Online Solubility Prediction Tool</h1>", unsafe_allow_html=True)
     #st.markdown("<h1 style='font-size: 34px ;position: fixed;  top: 0.1; width: 100%; color: blue; margin-top: 0.5; padding-top: 0.5;'>AqSolPred: Online Solubility Prediction Tool</h1>", unsafe_allow_html=True)
