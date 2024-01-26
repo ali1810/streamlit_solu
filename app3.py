16b70032913ceedcb3e0acb170cd12b2a1e7ddf4
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-01-26 15:59:17 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -107,7 +107,7 @@ def page1():
     
     # calculate the aromatic proportion descriptor
          #single_AP = getAromaticProportion(mol1)
-	  aromatic_list = [mol1.GetAtomWithIdx(i).GetIsAromatic() for i in range(m.GetNumAtoms())]
+	  aromatic_list = [mol1.GetAtomWithIdx(i).GetIsAromatic() for i in range(mol1.GetNumAtoms())]
           aromatic = 0
           for i in aromatic_list:
              if i:
