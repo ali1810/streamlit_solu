ac15774c5b9d0d34082faa1e99da9dca42e02cba
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-06 00:37:32 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -121,13 +121,19 @@ def page2():
     #st.write("This is the page for project details")
     image = Image.open('flow5.jpeg')
     #image_path = 'flow5.jpeg'
-    col1, col2, col3 = st.columns([0.001,2,1])
+    col1, mid, col2 = st.columns([15,0.5,5])
     with col1:
-	    st.write("")
+           st.image(image, use_column_width=False)
     with col2:
-	    st.image(image, use_column_width=2)
-    with col3:	
-            st.write("")
+           #showmol(xyzview,height=300,width=400)
+	    st.write("")
+    col1, col2, col3 = st.columns([0.001,2,1])
+    #with col1:
+	#    st.write("")
+    #with col2:
+	#    st.image(image, use_column_width=2)
+    #with col3:	
+     #       st.write("")
     #st.markdown(
      #   f'<div style="display: flex; justify-content: center;">'
       #  f'<img src="{image_path}" style="width: 50%; height: auto;">'
