f76f8375d27c8a0ca30f77ae57cdea730a8f7517
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-06 00:24:11 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -121,11 +121,11 @@ def page2():
     #st.write("This is the page for project details")
     image = Image.open('flow5.jpeg')
     #image_path = 'flow5.jpeg'
-    col1, col2, col3 = st.columns([0.001,3.0,0.1])
+    col1, col2, col3 = st.columns([1,2,1])
     with col1:
-	    st.write("")
+	    st.image(image, use_column_width=3)
     with col2:
-            st.image(image, use_column_width=3)
+	    st.write("")
     with col3:	
             st.write("")
     #st.markdown(
