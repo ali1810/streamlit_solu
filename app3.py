baa765db426263d47879c8a0e2483e9ad933fe9f
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-08 00:40:54 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -46,31 +46,31 @@ def page5():
     st.sidebar.write('**Type SMILES below**')
     smiles = st.sidebar.text_input('then press predict button', value ="CC(=O)OC1=CC=CC=C1C(=O)O")
     compounds = pcp.get_compounds(smiles, namespace='smiles')
-        match = compounds[0]
-        c_name=match.iupac_name
-        mol = Chem.MolFromSmiles(smiles)
-        mol = Chem.AddHs(mol)
-        AllChem.EmbedMolecule(mol)
-        mblock = Chem.MolToMolBlock(mol)
+    match = compounds[0]
+    c_name=match.iupac_name
+    mol = Chem.MolFromSmiles(smiles)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    mblock = Chem.MolToMolBlock(mol)
         #return mblock
-        xyzview = py3Dmol.view(width=400,height=300)
+    xyzview = py3Dmol.view(width=400,height=300)
     #xyzview = py3Dmol.view(query=′pdb:1A2C′)
-        xyzview.addModel(xyz,'mol')
-        xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
+    xyzview.addModel(xyz,'mol')
+    xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
     #bcolor = st.sidebar.color_picker('Pick background Color', '#0C0C0B')
-        style = st.sidebar.selectbox('Chemical structure',['stick','ball-and-stick','line','cross','sphere'])
+    style = st.sidebar.selectbox('Chemical structure',['stick','ball-and-stick','line','cross','sphere'])
 #spin = st.sidebar.checkbox('Spin', value = False)
-        spin = st.sidebar.checkbox('Animation', value = True)
+    spin = st.sidebar.checkbox('Animation', value = True)
+    xyzview.spin(True)
+    if spin:
         xyzview.spin(True)
-        if spin:
-            xyzview.spin(True)
-        else:
-            xyzview.spin(False)
+    else:
+	xyzview.spin(False)
     #xyzview.setStyle({'sphere':{}})
-        xyzview.setBackgroundColor('#EAE5E5')
-        xyzview.zoomTo()
-        xyzview.setStyle({style:{'color':'spectrum'}})
-        showmol(xyzview,height=300,width=400) 
+    xyzview.setBackgroundColor('#EAE5E5')
+    xyzview.zoomTo()
+    xyzview.setStyle({style:{'color':'spectrum'}})
+    showmol(xyzview,height=300,width=400) 
 
 
 ## Calculate molecular descriptors
