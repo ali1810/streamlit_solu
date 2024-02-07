3b5243524e9f43b0e1bee3f5a74c002a92a1f678
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-07 23:01:45 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -261,16 +261,16 @@ def remove_invalid(smiles):
 #st.set_page_config(page_title="AqSolPred: Online Solubility Prediction Tool")
 st.set_page_config(page_title="AqSolPred: Online Solubility Prediction Tool",layout="wide")
 st.write("""# Solibility Prediction on Aqueous Solvent """)
-image = Image.open('Flow.jpeg')
-col1, col2, col3 = st.columns([0.5,2.0,0.5])
-with col1:
-	st.write("")
+#image = Image.open('Flow.jpeg')
+#col1, col2, col3 = st.columns([0.5,2.0,0.5])
+#with col1:
+#	st.write("")
 
-with col2:
-	
-        st.image(image, use_column_width=6)
-with col3:	
-        st.write("")
+#with col2:
+#	
+ #       st.image(image, use_column_width=6)
+#with col3:	
+        #st.write("")
 	
 	
 	
