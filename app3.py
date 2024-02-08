95183177ace616dc4366a6b15a5da39136ca0606
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-09 00:55:29 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -57,23 +57,23 @@ def page1():
  #       st.write(" 3 D Structure  of the smiles")
  #       st.write("""Use mouse pointer to rotate the structure""")
 
-     prop=pcp.get_properties([ 'MolecularWeight'], SMILES, 'smiles')
-     x = list(map(lambda x: x["CID"], prop)) 
-     y=x[0]
+      prop=pcp.get_properties([ 'MolecularWeight'], SMILES, 'smiles')
+      x = list(map(lambda x: x["CID"], prop)) 
+      y=x[0]
     #print(y) 
-    x = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/PNG?image_size=400x300"
-    url=(x % y)
+      x = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/PNG?image_size=400x300"
+      url=(x % y)
 #print(url)
-    img = Image.open(urlopen(url))
-    mol = Chem.MolFromSmiles(SMILES)
-    mol = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol)
-    mblock = Chem.MolToMolBlock(mol)
-    xyzview = py3Dmol.view(width=400,height=300)
+      img = Image.open(urlopen(url))
+      mol = Chem.MolFromSmiles(SMILES)
+      mol = Chem.AddHs(mol)
+      AllChem.EmbedMolecule(mol)
+      mblock = Chem.MolToMolBlock(mol)
+      xyzview = py3Dmol.view(width=400,height=300)
     #xyzview = py3Dmol.view(query=′pdb:1A2C′)
-    xyzview.addModel(mblock,'mol')
-    xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
-    style = 'stick'
+      xyzview.addModel(mblock,'mol')
+      xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
+      style = 'stick'
 #bcolor = st.sidebar.color_picker('Pick background Color', '#0C0C0B')
 #xyzview.spin(True)
 #if spin:
@@ -81,18 +81,18 @@ def page1():
 #else:
  #    xyzview.spin(False)
     #xyzview.setStyle({'sphere':{}})
-    xyzview.setBackgroundColor('#EAE5E5')
-    xyzview.zoomTo()
-    xyzview.setStyle({style:{'color':'spectrum'}})
+      xyzview.setBackgroundColor('#EAE5E5')
+      xyzview.zoomTo()
+      xyzview.setStyle({style:{'color':'spectrum'}})
     
 
 
-    col1, mid, col2 = st.columns([15,2.5,15])
+      col1, mid, col2 = st.columns([15,2.5,15])
 #col1, mid, col2 = st.columns([15,0.5,15])
-    with col1:
-        st.image(img, use_column_width=False)
-    with col2:
-        showmol(xyzview,height=300,width=400) 
+      with col1:
+          st.image(img, use_column_width=False)
+      with col2:
+          showmol(xyzview,height=300,width=400) 
 
 #def page1():   
    
