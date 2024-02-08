88d8942673e60857af38ad9abeef57e1520912db
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-09 00:08:40 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -43,44 +43,42 @@ from bs4 import BeautifulSoup
 
 import streamlit as st
 
-def page1():
-    st.write('**Type SMILES below**')
-    SMILES = st.text_input('then press predict button', value ="CC(=O)OC1=CC=CC=C1C(=O)O")
-    style = st.selectbox('Chemical structure',['stick','ball-and-stick','line','cross','sphere'])
-    spin = st.checkbox('Spin', value = False)	
-    col1, col2, col3 = st.columns([10,2,11.5])
-
-    with col1:
-	    st.header("   2 D Structure of the smiles  ")
-    with col2:
-	    st.write("")
-    with col3:
-            st.header(" 3 D Structure  of the smiles")
-            st.write("""Use mouse pointer to rotate the structure""")
-    prop=pcp.get_properties([ 'MolecularWeight'], SMILES, 'smiles')
-    x = list(map(lambda x: x["CID"], prop))
-    y=x[0]
-    #print(y)
-    x = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/PNG?image_size=400x300"
-    url=(x % y)
+st.write('**Type SMILES below**')
+SMILES = st.text_input('then press predict button', value ="CC(=O)OC1=CC=CC=C1C(=O)O")
+style = st.selectbox('Chemical structure',['stick','ball-and-stick','line','cross','sphere'])
+spin = st.checkbox('Spin', value = False)	
+col1, col2, col3 = st.columns([10,2,11.5])
+with col1:	
+	st.header("   2 D Structure of the smiles  ")
+with col2:
+	st.write("")
+with col3:
+        st.header(" 3 D Structure  of the smiles")
+        st.write("""Use mouse pointer to rotate the structure""")
+prop=pcp.get_properties([ 'MolecularWeight'], SMILES, 'smiles')
+x = list(map(lambda x: x["CID"], prop)] 
+y=x[0]
+    #print(y) 
+x = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/%s/PNG?image_size=400x300"
+url=(x % y)
 #print(url)
-    img = Image.open(urlopen(url))
-    mol = Chem.MolFromSmiles(SMILES)
-    mol = Chem.AddHs(mol)
-    AllChem.EmbedMolecule(mol)
-    mblock = Chem.MolToMolBlock(mol)	
-    xyzview = py3Dmol.view(width=400,height=300)
+img = Image.open(urlopen(url))
+mol = Chem.MolFromSmiles(SMILES)
+mol = Chem.AddHs(mol)
+AllChem.EmbedMolecule(mol)
+mblock = Chem.MolToMolBlock(mol)	
+xyzview = py3Dmol.view(width=400,height=300)
     #xyzview = py3Dmol.view(query=′pdb:1A2C′)
-    xyzview.addModel(mblock,'mol')
-    xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
+xyzview.addModel(mblock,'mol')
+xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
     #bcolor = st.sidebar.color_picker('Pick background Color', '#0C0C0B')
     
-    spin = st.sidebar.checkbox('Animation', value = True)
+spin = st.sidebar.checkbox('Animation', value = True)
+xyzview.spin(True)
+if spin:
     xyzview.spin(True)
-    if spin:
-      xyzview.spin(True)
-    else:
-      xyzview.spin(False)
+else:
+    xyzview.spin(False)
     xyzview.setStyle({'sphere':{}})
     xyzview.setBackgroundColor('#EAE5E5')
     xyzview.zoomTo()
@@ -91,13 +89,15 @@ def page1():
 	
     # img=smiles_to_img(smiles)
 #st.write("a logo and text next to eachother")
-    col1, mid, col2 = st.columns([15,0.5,15])
-    with col1:
-           st.image(img, use_column_width=False)
-    with col2:
-           showmol(xyzview,height=300,width=400) 
+col1, mid, col2 = st.columns([15,0.5,15])
+with col1:
+	st.image(img, use_column_width=False)
+with col2:
+        showmol(xyzview,height=300,width=400) 
            #render_mol(blk)
 
+#def page1():
+    
    
 
 
