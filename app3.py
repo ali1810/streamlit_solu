5edad8d482271bc7a304419bcd9ae09894457d41
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-09 00:12:50 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -73,17 +73,17 @@ xyzview.addModel(mblock,'mol')
 xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
     #bcolor = st.sidebar.color_picker('Pick background Color', '#0C0C0B')
     
-spin = st.sidebar.checkbox('Animation', value = True)
+spin = st.checkbox('Animation', value = True)
 xyzview.spin(True)
 if spin:
     xyzview.spin(True)
 else:
     xyzview.spin(False)
-    xyzview.setStyle({'sphere':{}})
-    xyzview.setBackgroundColor('#EAE5E5')
-    xyzview.zoomTo()
-    xyzview.setStyle({style:{'color':'spectrum'}})
-    #showmol(xyzview,height=300,width=400) 
+xyzview.setStyle({'sphere':{}})
+xyzview.setBackgroundColor('#EAE5E5')
+xyzview.zoomTo()
+xyzview.setStyle({style:{'color':'spectrum'}})
+showmol(xyzview,height=300,width=400) 
     
 
 	
