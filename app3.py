1e0133ef28034e680af8baeb2a1cc0977e567e81
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-10 13:11:42 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -95,7 +95,7 @@ def page1():
           showmol(xyzview,height=300,width=400) 
       #if st.button("predict"):
       if st.button("Predict"):
-	      st.write("This is some content that should remain on the page.")
+	 st.write("This is some content that should remain on the page.")
         	      
           #page1()
       #        st.write("work in Progress") 
@@ -186,6 +186,7 @@ def main():
         # Add more content to the container dynamically
         with container:
            st.write("Additional content added!")
+           page1()		
                #st.write("This content is added dynamically without affecting the existing content.")
 		    
 	 
