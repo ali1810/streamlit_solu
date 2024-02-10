2bd73f2083c6fedad93b7cec3de610c1966d25f6
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-10 16:30:03 +0100

    Update app3.py

--- app3.py
+++ app3.py
@@ -147,7 +147,7 @@ def main():
     
     # Create a container to hold the content
     if button1_clicked:
-	    container = st.container()
+	    #container = st.container()
 	    page1()    
     
     # Initial content on the page
