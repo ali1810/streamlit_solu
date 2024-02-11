f9d94000ff5831c0dc8fad2ad061d666f53e0aab
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-11 22:30:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+
 import contact,home,project
+
 st.set_page_config(
         page_title="Solubility Prediction",
-)
+         )
 
 
 
