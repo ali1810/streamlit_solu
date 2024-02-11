71d74966491ecfebcc2e3068913bfe7f61a2a54d
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-11 21:09:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,41 +2,52 @@ import streamlit as st
 
 from streamlit_option_menu import option_menu
 
-import contact,home,project
 
-st.set_page_config(page_title="Solubility Prediction",)
+import home, trending, test, your, about
+st.set_page_config(
+        page_title="Pondering",
+)
+
+
 
 class MultiApp:
-  def _init_(self):
-    self.apps=[]
-  def add_app(self,title,function):
-    seld.apps.append({ "title":title,"function":function})
 
-  def run():
-    with st.sidebar:
-      app=option_menu(menu_titel='solubility prediction', options=['Home','Project','Contact'],
-                      icons=['house-fill','person-circle','chat-fill'],
-                      menu_icon ='chat-text-fill',
-                      default_index=1, 
-                      styles= {"container":{"padding":"5!important" ,"background-color":"black"},
-                                            "icon":{"color":"white","font-size":"23px"},
-                                             "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
-                                             "nav-link-selected": {"background-color": "#02ab21"},}
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run():
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='Pondering ',
+                options=['Home','Project','ContactTrending'],
+                icons=['house-fill','person-circle','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'black'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
                 
+                )
 
-                                            )
-    if app == "Home":
-          home.app()
-    if app == "Project":
-          project.app()    
-    if app == "Contact":
-          contact.app()        
-        #if app == 'Your Posts':
-         #   your.app()
-        #if app == 'about':
-         #   about.app()    
-             
+        
+        if app == "Home":
+            home.app()
+        if app == "Project":
+            project.app()    
+        if app == "Contact":
+            contact.app()        
+        
           
              
-  run()            
+    run()            
          
