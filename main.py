2f3a0293782616be30059335cef9fe2abdf03763
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-11 14:23:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import streamlit as st 
 
+import streamlit_option_menu import option_menu
+
+import contact,home,project
+
+st.set_page_config(page_titel="Solubility Prediction",)
+
+class MultiApp:
+  def _init_(self):
+    self.apps=[]
+  def add_app(self,titel,function):
+    seld.apps.append({ "titel":titel,"function":function})
+
+  def run():
+    with st.sidebar:
+      app=option_menu(menu_titel='solubility prediction', options=['home','project','contact'],
+                      icons=['house-fill','person-circle','contact-fill']
+                      menu_icon ='chat-text-fill',
+                      default_index=1, 
+                      styles= {"container":{"padding":"5!important" ,"background-color":'black'
+                                            "icon":{"color":"white","font-size","20px","text-align":"left",
+                                                    "nav-link_selected":{"background-color":"#02ab21"}}
+                                            )
+    
