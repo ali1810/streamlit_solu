3a08eabc34b21075d0c25b169f145b14cb7a9135
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-11 17:28:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class MultiApp:
   def run():
     with st.sidebar:
       app=option_menu(menu_titel='solubility prediction', options=['home','project','contact'],
-                      icons=['house-fill','person-circle','contact-fill']
+                      icons=['house-fill','person-circle','chat-fill']
                       menu_icon ='chat-text-fill',
                       default_index=1, 
                       styles= {"container":{"padding":"5!important" ,"background-color":'black'
