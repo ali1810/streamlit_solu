3d4f46d58f7b253c289694151f1ba54c22f1907a
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-12 00:03:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ class MultiApp:
             app = option_menu(
                 menu_title='Solubility Prediction ',
                 options=['Home','Project','Contact'],
-                icons=['house-fill','person-circle','info-circle-fill'],
+                icons=['house-fill','person-circle',':phone:'],
                 menu_icon='chat-text-fill',
                 default_index=1,
                 styles={
