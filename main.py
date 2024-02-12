e2537079eff66fe8a0d0b5bc3fad4066152233df
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-12 18:08:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class MultiApp:
                 styles={
                     "container": {"padding": "5!important","background-color":'white'},
         "icon": {"color": "black", "font-size": "23px"}, 
-        "nav-link": {"color":"black","font-size": "20px", "text-align": "left", "margin":"1px", "--hover-color": "blue"},
+        "nav-link": {"color":"black","font-size": "15px", "text-align": "left", "margin":"1px", "--hover-color": "blue"},
         "nav-link-selected": {"background-color": "#02ab21"},}
                 
                 )
