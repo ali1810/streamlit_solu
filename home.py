81dcc63a9c82e86d337f851c4d50a1eda8f5dcf4
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-12 13:41:45 +0100

    Update home.py

--- home.py
+++ home.py
@@ -96,19 +96,6 @@ def app():
 	#      st.write("This is some content that should remain on the page.")        	      
           #page1()
       #        st.write("work in Progress") 
-      prop1=pcp.get_properties([ 'MolecularWeight'], SMILES, 'smiles')
-      x1 = list(map(lambda x: x["CID"], prop1))
-      y1=x1[0]
-      x1 = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/%s/xml"
-      data1=requests.get(x1 % y1)
-      html = BeautifulSoup(data1.content, "xml")
-      solubility = html.find(name='TOCHeading', string='Solubility')
-      if solubility ==None:
-             Sol= None
-      else:
-	      
-	  solub=solubility.find_next_sibling('Information').find(name='String').string
-          Sol=solub
       if st.button("Predict"):
 	  
 
