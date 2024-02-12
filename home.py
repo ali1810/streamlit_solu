160c1d7e1537d0cf7e13e762ffb55653d7d50155
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-12 15:07:53 +0100

    Update home.py

--- home.py
+++ home.py
@@ -98,8 +98,20 @@ def app():
       #        st.write("work in Progress") 
       if st.button("Predict"):
 	      prop=pcp.get_properties([ 'MolecularWeight'], SMILES, 'smiles')
-              #x = list(map(lambda x: x["CID"], prop))
-              #y=x[0]
+              x = list(map(lambda x: x["CID"], prop))
+              y=x[0]
+              x = "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/%s/xml"
+              data=requests.get(x % y)
+    
+              html = BeautifulSoup(data.content, "xml")
+              solubility = html.find(name='TOCHeading', string='Solubility')
+              #if solubility ==None:
+               #     return None
+#sol.append(solub)
+             #else:
+              #   solub=solubility.find_next_sibling('Information').find(name='String').string
+               #  return solub
+
 
 	      
 	      
