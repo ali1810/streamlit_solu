eac1d4f9bfa27530d007ea83da712f529fe18b8b
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-15 23:42:51 +0100

    Update 01_🏠 home.py

--- pages/01_🏠 home.py
+++ pages/01_🏠 home.py
@@ -74,7 +74,7 @@ xyzview = py3Dmol.view(width=300,height=300)
 xyzview.addModel(mblock,'mol')
 xyzview.setStyle({'model': -1}, {"cartoon": {'color': 'spectrum'}})
 style = 'stick'
-spin = st.checkbox('Animation', value = True) style = st.selectbox('Chemical structure',['stick','ball-and-stick','line','cross','sphere'])
+spin = st.checkbox('Animation', value = True) style = st.selectbox('Chemical structure',['stick','ball-and-stick','line','cross','sphere']) 
 
 #bcolor = st.sidebar.color_picker('Pick background Color', '#0C0C0B')
 xyzview.spin(True)
