9f4a3a8b2b47af34c702350dc4cc12f9eb02cf7b
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-02-18 20:45:09 +0100

    Update 01_ Home.py

--- pages/01_ Home.py
+++ pages/01_ Home.py
@@ -43,7 +43,7 @@ from bs4 import BeautifulSoup
 #col1, mid, col2 = st.columns([15,2.5,15])
 #col1, mid, col2 = st.columns([5,2.0,5])
 #with col1:
-st.header("---------Aquous Solubility Prediction---------")
+st.header("-----------Aquous Solubility Prediction-----------")
 #with col2:
         #st.write() 
       #if st.button("predict"):
